a3bad495ce7d54031320d77da20d4fe79a12a0fb
edmundo papi <edmundopierre@yahoo.com>
2020-11-23 14:55:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+function countDown (number) {
+
+for(let i = 0; i<=11; i--){
+    console.log(`${i}`);
+  }
+};
+
+countDown(number);
